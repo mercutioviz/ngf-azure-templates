b0b74d81602782c07043d0f26113b774c8ad8629
Michael S Collins <mercutio.viz@gmail.com>
2018-09-13 14:13:15 -0700

    Add post-install.sh script so that the ngfCustomData string isn't so obnoxiously lengthy

---
+++ NGF-Quickstart-HA-1NIC-AS-ELB-ILB-STD/post-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl -s https://raw.githubusercontent.com/mercutioviz/ngf-azure-templates/master/NGF-Quickstart-HA-1NIC-AS-ELB-ILB-STD/simple-ha.fwrule > /tmp/template.fwrule
+red=$1
+green=$2
+echo "Red subnet: $1" >> /tmp/post-install.log
+echo "Green subnet: $2" >> /tmp/post-install.log
+perl -pi -e "s#placeholder_azure_subnet_red#${red}RMASK#g" /tmp/template.fwrule
+perl -pi -e "s#placeholder_azure_subnet_green#${green}RMASK#g" /tmp/template.fwrule
+perl -pi -e 's#(\d+)RMASK#(32 - $1)#e' /tmp/template.fwrule
+acpfrule check /tmp/active.fwrule && cp /tmp/active.fwrule /opt/phion/config/configroot/servers/S1/services/NGFW/ && cp /tmp/active.fwrule /opt/phion/config/active/servers/S1/services/NGFW/ && /opt/phion/modules/server/firewall/bin/activate
