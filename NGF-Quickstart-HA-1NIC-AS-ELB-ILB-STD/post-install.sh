406f84baa1fef5845fbb435610a64be48746fbe5
Michael S Collins <mercutio.viz@gmail.com>
2018-09-13 14:25:34 -0700

    Adjust ngfCustomData1 and put CLI args in right place

--- NGF-Quickstart-HA-1NIC-AS-ELB-ILB-STD/post-install.sh
+++ NGF-Quickstart-HA-1NIC-AS-ELB-ILB-STD/post-install.sh
@@ -8,3 +8,4 @@ perl -pi -e "s#placeholder_azure_subnet_red#${red}RMASK#g" /tmp/template.fwrule
 perl -pi -e "s#placeholder_azure_subnet_green#${green}RMASK#g" /tmp/template.fwrule
 perl -pi -e 's#(\d+)RMASK#(32 - $1)#e' /tmp/template.fwrule
 acpfrule check /tmp/active.fwrule && cp /tmp/active.fwrule /opt/phion/config/configroot/servers/S1/services/NGFW/ && cp /tmp/active.fwrule /opt/phion/config/active/servers/S1/services/NGFW/ && /opt/phion/modules/server/firewall/bin/activate
+sleep 1
