7c5a79b808207f45ce1be88c48ed205a630f2cbc
Michael S Collins <mercutio.viz@gmail.com>
2018-09-14 17:24:17 -0700

    Initial commit of powershell CGF installer script

---
+++ NGF-Quickstart-HA-1NIC-AS-ELB-ILB-STD/AzureBN-Install.psm1
@@ -0,0 +1,101 @@
+﻿# Generic user choice menu
+# Supply an object list and function creates numbered list of choices for user to select from
+# Supply an optional secondary value field if the primary field isn't what is being selected
+
+function UserChoice {
+param($objMenuSource,$valSelectionField, $strPrompt, $valSecondaryField)
+    $menu = @{}
+    for ($i=1;$i -le $objMenuSource.count; $i++) {
+        $menuItem = $objMenuSource[$i-1].$valSelectionField
+        if ( $valSecondaryField ) {
+            $menuItem = "{0,-30} : {1,-30}" -f $menuItem,$objMenuSource[$i-1].$valSecondaryField
+        }
+        $menu.Add($i,($menuItem))
+        Write-Host("{0,2}) {1}" -f $i,$menuItem)
+    }
+
+    do {
+        [int]$ans = Read-Host $strPrompt
+        if ( $ans -gt $objMenuSource.count -or $ans -lt 1 ) {
+            Write-Host("Invalid entry, please enter a number between 1 and {0}" -f $objMenuSource.count) -ForegroundColor DarkYellow
+        }
+    } while ($ans -lt 1 -or $ans -gt $objMenuSource.count )
+    
+    if ( $valSecondaryField ) {
+         $selection = $objMenuSource[$ans-1].$valSecondaryField
+    } else {
+         $selection = $objMenuSource[$ans-1].$valSelectionField
+    }
+
+    $selection
+}
+
+# Check for Azure cmdlets
+function verifyAzureCmdlets {
+    $res = Get-Module PowerShellGet -list | Select-Object Name,Version,Path
+    return $res
+}
+
+# Validate CIDR
+function isCIDR {
+    Param(
+        #[ValidatePattern("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/(3[0-2]|[1-2][0-9]|[0-9]))$")]
+        $inputData
+        )
+    if ( $inputData -match '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/(3[0-2]|[1-2][0-9]|[0-9]))$' ) {
+        return $true
+    }
+}
+
+# Verbose print
+function vPrint {
+    Param($val)
+    if ( $Verbose ) { Write-Host $val }
+}
+
+# Check for subscription and prompt for login if not found
+function getSubs {
+$mysubs = Get-AzureRmSubscription -ErrorAction SilentlyContinue
+    if ( ! $mysubs ) {
+        vPrint("No subs found, running Login-AzureRmAccount")
+        Login-AzureRmAccount
+        $mysubs = Get-AzureRmSubscription -ErrorAction SilentlyContinue
+        if ( ! $mysubs ) {
+            Write-Host "Failed to find subscription, exiting..." -ForegroundColor Red
+            exit
+        } else {
+            vPrint("Found subs: " + $mysubs)
+        }
+    } else {
+        vPrint("Subs found: " + $mysubs)
+    }
+    return $mysubs
+}
+
+
+
+$vmTypes = @{'Standard_F1'     = @{ 'cores' = '1'; 'memory' = '2GB'};
+             'Standard_F1s'    = @{ 'cores' = '1'; 'memory' = '2GB'};
+             'Standard_DS1'    = @{ 'cores' = '1'; 'memory' = '3.5GB'};
+             'Standard_DS1_v2' = @{ 'cores' = '1'; 'memory' = '3.5GB'};
+             'Standard_F2'     = @{ 'cores' = '2'; 'memory' = '4GB'};
+             'Standard_F2s'    = @{ 'cores' = '2'; 'memory' = '4GB'};
+             'Standard_DS2'    = @{ 'cores' = '2'; 'memory' = '7GB'};
+             'Standard_DS2_v2' = @{ 'cores' = '2'; 'memory' = '7GB'};
+             'Standard_F4'     = @{ 'cores' = '4'; 'memory' = '8GB'};
+             'Standard_F4s'    = @{ 'cores' = '4'; 'memory' = '8GB'};
+             'Standard_DS3'    = @{ 'cores' = '4'; 'memory' = '14GB'};
+             'Standard_DS3_v2' = @{ 'cores' = '4'; 'memory' = '14GB'};
+             'Standard_F8'     = @{ 'cores' = '8'; 'memory' = '16GB'};
+             'Standard_F8s'    = @{ 'cores' = '8'; 'memory' = '16GB'};
+             'Standard_DS4'    = @{ 'cores' = '8'; 'memory' = '28GB'};
+             'Standard_DS4_v2' = @{ 'cores' = '8'; 'memory' = '28GB'};
+            }
+
+$skuTypes = @{'hourly' = 'PAYG Hourly (Includes compute and license)';
+              'byol'   = 'BYOL (Bring Your Own License) - request license token from barracuda.com/eval)';
+              }
+
+# Export it all
+Export-ModuleMember -Variable '*'
+Export-ModuleMember -Function '*'
---
+++ NGF-Quickstart-HA-1NIC-AS-ELB-ILB-STD/deploynew.ps1
@@ -0,0 +1,211 @@
+﻿#################################################################
+#
+#  AZURE DEPLOY CGF
+#  by
+#  Michael S Collins
+#  mcollins@barracuda.com
+#
+#  Deploys 1 or 2 CGFs depending on user choice.
+#   User may choose new or existing infrastructure
+#
+#  NO WARRANTY OR GUARANTEE OF ANY KIND, EXPRESS OR IMPLIED
+#
+#################################################################
+
+### Specify parameters/flags
+Param(
+    [switch]$SkipAzure = $false,
+    [switch]$Verbose   = $false,
+    [switch]$Help      = $false,
+    [int]$var1         = '1',
+    [String]$var2      = 'myValue'
+)
+
+Import-Module AzureBN-Install
+
+# Check for help first before doing anything else
+if ( $Help ) {
+    Write-Host "Help for program XXXX"
+    exit
+}
+# Main program area
+if ( ! $SkipAzure ) {
+    vPrint("Checking for Azure cmdlets...")
+    $azurecmdlets = verifyAzureCmdlets
+    if ( ! $azurecmdlets ) {
+        Write-Host "Azure cmdlets not found on this system" -ForegroundColor Red
+        exit
+    } else {
+        vPrint("Found Azure cmdlets")
+    }
+} else {
+    vPrint "Skipping Azure cmdlets check"
+}
+
+#====================================================
+# Install into new VNET
+function installIntoNewVNet {
+    $vnetName = Read-Host "Please enter new VNet name (<enter>=CGF-VNet)"
+    if ( ! $vnetName )
+    {
+        $vnetName = 'CGF-VNet'
+    }
+
+    do {
+        $vnetAddrSpace = Read-Host "Please enter new VNet address space (<enter>=172.16.136.0/20)"
+        if ( ! $vnetAddrSpace ) {
+            $vnetAddrSpace = '172.16.136.0/20'
+        } else {
+            $CIDROK = isCIDR($vnetAddrSpace)
+            if ( ! $CIDROK  ) {
+                Write-Host "Sorry, '$vnetAddrSpace' appears not to be a valid CIDR. Please try again." -ForegroundColor Yellow
+                $vnetAddrSpace = ''
+            }
+        }
+    } while ( ! $vnetAddrSpace )
+    $vnetName,$vnetAddrSpace
+}
+
+# Install into existing VNET
+function installIntoExistingVNet {
+    Write-Host "`n`nAvailable Virtual Networks in this subscription:"
+    $sel = UserChoice $vnets 'addrSpace' 'Please choose VNet for deployment' 'Name' 
+
+    $sel
+}
+
+# Add a new resource group
+function addNewRG {
+    $rgName = Read-Host "Please enter new resource group name (<enter>=CloudGenFW-RG)"
+    if ( ! $rgName ) {
+        $rgName = 'CloudGenFW-RG'
+    }
+
+    $rgName
+}
+
+# Get existing resource group
+function getExistingRG {
+    Write-Host "Gathering resource group information..." -ForegroundColor Cyan
+    $rgs = Get-AzureRmResourceGroup | Select-Object ResourceGroupName
+    $sel = UserChoice $rgs 'ResourceGroupName' 'Please choose Resource Group for deployment'
+
+    $sel
+}
+#====================================================
+
+## Global variables used in building template
+$vnets = ''
+$vnetName = ''
+$vnetAddrSpace = ''
+$rgName = ''
+$fwsubnetName = ''
+$fwsubnetCIDR = ''
+$privateSubnet1Name = ''
+$privateSubnet1CIDR = ''
+$privateSubnet2Name = ''
+$privateSubnet1CIDR = ''
+$vmSize = ''
+$adminPassword = ''
+$fw1VMName = ''
+$fw2VMName = ''
+$fw1IPAddr = ''
+$fw2IPAddr = ''
+$imageSKU = ''
+
+
+Write-Host "Barracuda CloudGen Firewall Installer Script. Press <enter> to continue, any other key to quit" -ForegroundColor Cyan
+$sel = Read-Host -Prompt "`nEnter selection "
+if ( $sel ) { 
+    Write-Host "Exiting at user direction..." 
+    exit
+}
+
+Write-Host "`nCollecting subscription information..." -ForegroundColor Cyan
+$mysubs = getSubs
+$sub = ''
+if ( $mysubs.count -eq '1' ) {
+    Write-Host ('For account {0} we will use SubscriptionId {1}' -f $mysub.Name, $mysub.Id)
+} else {
+    # Select a subscription to use
+    $sub = UserChoice $mysubs 'Id' 'Please select subscription for this operation' 'Name'
+}
+
+Write-Host ("`nGathering stats for subscription ID: {0}" -f $sub) -ForegroundColor Cyan
+Write-Host "Gathering VNet information..." -ForegroundColor Cyan
+$vnets = Get-AzureRmVirtualNetwork | Select-Object Name,@{n='AddrSpace'; e={$_.AddressSpace.AddressPrefixes}}
+
+# New or existing VNet
+do {
+    $sel = Read-Host "`n(N)ew or (E)xisting VNet? (N/E/Q=quit/<enter>=New)"
+    if ( ! $sel -or $sel -imatch '^N' ) {
+        $installType = 'New'
+    } elseif ( $sel -imatch '^E' ) {
+        $installType = 'Existing'
+    } elseif ( $sel -imatch '^Q' ) {
+        Write-Host "Exiting program at user request..." -ForegroundColor Yellow
+        exit
+    } else {
+        Write-Host "Invalid selection, please try again" -ForegroundColor DarkYellow
+    }
+} while ( ! $installType )
+
+if ( $installType -eq 'New' ) {
+    $res = installIntoNewVNet
+    $vnetName = $res[0]
+    $vnetAddrSpace = $res[1]
+    Write-Host "Will create new VNet $vnetName with address space of $vnetAddrSpace" -ForegroundColor Cyan
+} else {
+    $vnetName = installIntoExistingVNet
+    $vnetAddrSpace = $vnets.Where({$_.Name -eq $vnetName}).AddrSpace
+    Write-Host "Will use existing VNet $vnetName with address space of $vnetAddrSpace" -ForegroundColor Cyan
+}
+
+# New or existing resource group
+do {
+    $sel = Read-Host "`n(N)ew or (E)xisting Resource Group? (N/E/Q=quit/<enter>=New)"
+    if ( ! $sel -or $sel -imatch '^N' ) {
+        $rgType = 'New'
+    } elseif ( $sel -imatch '^E' ) {
+        $rgType = 'Existing'
+    } elseif ( $sel -imatch '^Q' ) {
+        Write-Host "Exiting program at user request..." -ForegroundColor Yellow
+        exit
+    } else {
+        Write-Host "Invalid selection, please try again" -ForegroundColor DarkYellow
+    }
+} while ( ! $rgType )
+
+if ( $rgType -eq 'New' ) {
+    $rgName = addNewRG
+    Write-Host "Will create new resource group $rgName" -ForegroundColor Cyan
+} else {
+    $rgName = getExistingRG
+    Write-Host "Using existing resource group $rgName" -ForegroundColor Cyan
+}
+
+
+# How many to install
+do {
+    Write-Host "`nInstallation Type"
+    $sel = Read-Host "Single FW or High Availablity (HA) pair? (s/h/Q=Quit/<enter>=HA)"
+    if ( ! $sel -or $sel -imatch '^H' ) {
+        $numFirewalls = '2'
+    } elseif ( $sel -imatch '^S' ) {
+        $numFirewalls = '1'
+    } elseif ( $sel -imatch '^Q' ) {
+        Write-Host "Exiting program at user request..." -ForegroundColor Yellow
+        exit
+    } else {
+        Write-Host "Invalid selection, please try again" -ForegroundColor DarkYellow
+    }
+} while ( ! $numFirewalls )
+
+Write-Host
+Write-Host "Installation Parameters"
+Write-Host "======================="
+Write-Host "VNet: $vnetName ($vnetAddrSpace)"
+Write-Host "  RG: $rgName"
+Write-Host " #FW: $numFirewalls"
+
+
